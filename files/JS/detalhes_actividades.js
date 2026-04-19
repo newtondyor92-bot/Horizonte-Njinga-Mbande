2b523b2fbfa829856daa6514c49718de812ac53e
FelixFernandes07 <felixalbertofernandes07@gmail.com>
2026-04-19 21:12:46 +0100

    Update detalhes_actividades.js

--- files/JS/detalhes_actividades.js
+++ files/JS/detalhes_actividades.js
@@ -1,7 +1,7 @@
 
 const dados = [
 {
-img:"imagem actividades/o enterro.jpg",
+img:"files/imagens/outras/o enterro.jpg",
 data:"🗓️ 17,18,19 de Abril de 2026",
 titulo:"O ENTERRO",
 desc:"A Hori – Njinga Mbande apresenta uma emocionante peça teatral que promete prender a tua atenção do início ao fim! O Enterro traz uma história forte, cheia de emoções, conflitos e reflexões sobre a vida e...",
@@ -9,14 +9,14 @@ link:"Ler mais"
 
 },
 {
-img:"imagem actividades/segredo da cama.jpg",
+img:"files/imagens/outras/segredo da cama",
 data:"🗓️ 2,3,5 de Abril 2026",
 titulo:"O SEGREDO DA CAMA",
 desc:"A Horizonte – Njinga Mbande apresenta mais uma grande produção teatral que vai surpreender o público: “O Segredo da Cama”.Uma história envolvente cheia de mistério, emoções e reviravoltas, que explora relações humanas, escolhas da vida e consequências inesperada",
 link:"Ler mais"
 },
 {
-img:"imagem actividades/Noticia  do site.png",
+img:"files/imagens/outras/o enterro.jpg",
 data:"🗓️ BREVEMENTE",
 titulo:"BREVEMENTE: SITE OFICIAL DA HORIZONTE NJINGA MBANDE!",
 desc:" 🌐Estamos a preparar algo grande para ti! Muito em breve, a Horizonte – Njinga Mbande vai lançar o seu site oficial, com um design moderno, elegante e totalmente intuitivo.",
@@ -24,14 +24,14 @@ link:"Ler mais"
 
 },
 {
-img:"imagem actividades/o enterro.jpg",
+img:"files/imagens/outras/o enterro.jpg",
 data:"🗓️ 17,18,19 de Abril de 2026",
 titulo:"O ENTERRO",
 desc:"A Horizonte – Njinga Mbande apresenta uma emocionante peça teatral que promete prender a tua atenção do início ao fim! O Enterro traz uma história forte, cheia de emoções, conflitos e reflexões sobre a vida e...",
 link:"Ler mais"
 },
 {
-img:"imagem actividades/segredo da cama.jpg",
+img:"files/imagens/outras/o enterro.jpg"",
 data:"🗓️ 2,3,5 de Abril 2026",
 titulo:"O SEGREDO DA CAMA",
 desc:"A Horizonte – Njinga Mbande apresenta mais uma grande produção teatral que vai surpreender o público: “O Segredo da Cama”.Uma história envolvente cheia de mistério, emoções e reviravoltas, que explora relações humanas, escolhas da vida e consequências inesperada",
@@ -40,7 +40,7 @@ link:"Ler mais"
 
 },
 {
-img:"imagem actividades/Noticia  do site.png",
+img:"files/imagens/outras/o enterro.jpg",
 data:"🗓️ BREVEMENTE",
 titulo:"BREVEMENTE: SITE OFICIAL DA HORIZONTE NJINGA MBANDE!",
 desc:" 🌐Estamos a preparar algo grande para ti! Muito em breve, a Horizonte – Njinga Mbande vai lançar o seu site oficial, com um design moderno, elegante e totalmente intuitivo.",
