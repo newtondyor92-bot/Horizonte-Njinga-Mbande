acd695a556dc68829d6ae7b34f9bebb821c0d7a4
FelixFernandes07 <felixalbertofernandes07@gmail.com>
2026-04-19 21:34:01 +0100

    Update actividade.js

--- files/JS/actividade.js
+++ files/JS/actividade.js
@@ -22,14 +22,14 @@ link:"Ler mais"
 
 },
 {
-img:"imagem actividades/o enterro.jpg",
+img:"imagens/outras/o enterro.jpg",
 data:"🗓️ 17,18,19 de Abril de 2026",
 titulo:"O ENTERRO",
 desc:"A Horizonte – Njinga Mbande apresenta uma emocionante peça teatral que promete prender a tua atenção do início ao fim! O Enterro traz uma história forte, cheia de emoções, conflitos e reflexões sobre a vida e...",
 link:"Ler mais"
 },
 {
-img:"imagem actividades/segredo da cama.jpg",
+img:"imagens/outras/o enterro.jpg",
 data:"🗓️ 2,3,5 de Abril 2026",
 titulo:"O SEGREDO DA CAMA",
 desc:"A Horizonte – Njinga Mbande apresenta mais uma grande produção teatral que vai surpreender o público: “O Segredo da Cama”.Uma história envolvente cheia de mistério, emoções e reviravoltas, que explora relações humanas, escolhas da vida e consequências inesperada",
@@ -37,7 +37,7 @@ link:"Ler mais"
 
 },
 {
-img:"imagem actividades/Noticia  do site.png",
+img:"imagens/outras/o enterro.jpg",
 data:"🗓️ BREVEMENTE",
 titulo:"BREVEMENTE: SITE OFICIAL DA HORIZONTE NJINGA MBANDE!",
 desc:" 🌐Estamos a preparar algo grande para ti! Muito em breve, a Horizonte – Njinga Mbande vai lançar o seu site oficial, com um design moderno, elegante e totalmente intuitivo.",
@@ -55,7 +55,7 @@ card.innerHTML = `
 <div class="card-title">${item.titulo}</div>
 <div class="card-desc"><strong>${item.desc}</strong></div>
 
-<a href="detalhes_actividades.html?id=${index}" class="link">
+<a href="files/Html/detalhes_actividades.html?id=${index}" class="link">
   ${item.link}
 </a>
 
